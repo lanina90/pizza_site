a5254366acd8a01947f09fd3e72fadd8d6662ad2
Kateryna Laina <boba4ok@gmail.com>
2023-05-20 15:42:23 +0300

    - useRef для инпута

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,26 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import styles from './Search.module.scss'
 import AppContext from "../../context/AppContext";
 
 const Search = () => {
 
   const {searchValue, setSearchValue} = useContext(AppContext)
+
+  const inputRef = useRef()
+
+  const onClickClear = () => {
+    setSearchValue('')
+    inputRef.current.focus()
+  }
+
+  useEffect(() => {
+    document.querySelector('input')
+  }, [])
   return (
     <div className={styles.root} >
       <img className={styles.icon} src="/img/search.svg" alt="Search"/>
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={e => setSearchValue(e.target.value) }
         type="text"
@@ -18,7 +30,7 @@ const Search = () => {
           className={styles.clearIcon}
           src="/img/clear_icon.svg"
           alt="Clear"
-          onClick={()=> setSearchValue('')}
+          onClick={()=> onClickClear()}
         /> : ''
       }
 
